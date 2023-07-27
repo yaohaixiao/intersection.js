13be884417177edeaf5abbff443549b091591d82
Yaohaixiao <yaohaixiao@gmail.com>
2023-07-28 07:20:49 +0800

    chore: 初始化项目代码

---
+++ intersection.js
@@ -0,0 +1,44 @@
+import isFunction from './utils/isFunction'
+import isElement from './utils/isElement'
+
+/**
+ * 通用的 IntersectionObserver 观察者处理器
+ * ========================================================================
+ * @method intersection
+ * @param {Function} fn
+ * @param {Object} [props]
+ * @param {Object|HTMLElement} [props.root]
+ * @param {String} [props.selector]
+ * @param {Object} [props.context]
+ * @param {String} [props.attr]
+ * @param {String} [props.rootMargin]
+ */
+const intersection = (fn, props = {}) => {
+  const root = props.root || null
+  const selector = props.selector || '.outline-heading'
+  const context = props.context || null
+  const rootMargin = props.rootMargin || '0px 0px -90% 0px'
+  const options = {
+    rootMargin: rootMargin
+  }
+  const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        if (isFunction(fn)) {
+          fn.call(context || entry.target, entry.target)
+        }
+      }
+    })
+  }, options)
+  const $root = isElement(root) ? root : document
+
+  if (root) {
+    options.root = root
+  }
+
+  $root.querySelectorAll(selector).forEach((section) => {
+    Observer.observe(section)
+  })
+}
+
+export default intersection
